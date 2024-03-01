a14f12bb853a4a4d5eb7ae2787b42e3f4da37144
Mateus <mateus.g.spencer@tecnico.ulisboa.pt>
2024-03-01 13:59:44 +0000

    client debugging option

--- Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/ClientMain.java
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/ClientMain.java
@@ -1,22 +1,32 @@
 package pt.ulisboa.tecnico.tuplespaces.client;
 
+import java.util.Arrays;
+
 import pt.ulisboa.tecnico.tuplespaces.client.grpc.ClientService;
 
 public class ClientMain {
     public static void main(String[] args) {
 
-        /*System.out.println(ClientMain.class.getSimpleName());
+        boolean debug = Arrays.asList(args).contains("-debug");
 
-        // receive and print arguments
-        System.out.printf("Received %d arguments%n", args.length);
-        for (int i = 0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
-        }*/
+        if(debug){
+            System.out.println("Debug mode enabled");
+
+            System.out.println(ClientMain.class.getSimpleName());
+
+            // receive and print arguments
+            System.out.printf("Received %d arguments%n", args.length);
+            for (int i = 0; i < args.length; i++) {
+                System.out.printf("arg[%d] = %s%n", i, args[i]);
+            }
+        }
+
+        
 
         // check arguments
-        if (args.length != 2) {
+        if ((debug && args.length != 3) || (!debug && args.length != 2)) {
             System.err.println("Argument(s) missing!");
-            System.err.println("Usage: mvn exec:java -Dexec.args=<host> <port>");
+            System.err.printf("Usage: mvn exec:java -Dexec.args=<host> <port> %s\n", debug ? "<-debug>" : "");
             return;
         }
 
@@ -24,7 +34,7 @@ public class ClientMain {
         final String host = args[0];
         final String port = args[1];
 
-        CommandProcessor parser = new CommandProcessor(new ClientService(host, port));
+        CommandProcessor parser = new CommandProcessor(new ClientService(host, port, debug));
         parser.parseInput();
 
     }
--- Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/grpc/ClientService.java
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/grpc/ClientService.java
@@ -9,14 +9,20 @@ import pt.ulisboa.tecnico.tuplespaces.centralized.contract.*;
 public class ClientService {
 
   private final TupleSpacesGrpc.TupleSpacesBlockingStub stub;
+  private boolean debug = false;
 
-  public ClientService(String host, String port) {
+  public ClientService(String host, String port, boolean debug) {
+    this.debug = debug;
     final String target = host + ":" + port;
     final ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
     this.stub = TupleSpacesGrpc.newBlockingStub(channel);
   }
 
   public void put(String tuple) {
+    if (debug) {
+      System.err.println("Putting tuple: " + tuple);
+    }
+
     PutRequest request = PutRequest.newBuilder().setTuple(tuple).build();
     stub.put(request);
 
@@ -24,6 +30,10 @@ public class ClientService {
   }
 
   public String read(String pattern) {
+    if (debug) {
+      System.err.println("Reading with pattern: " + pattern);
+    }
+
     ReadRequest request = ReadRequest.newBuilder().setPattern(pattern).build();
     ReadResponse response = stub.read(request);
 
@@ -32,6 +42,10 @@ public class ClientService {
   }
 
   public String take(String pattern) {
+    if (debug) {
+      System.err.println("Taking with pattern: " + pattern);
+    }
+
     TakeRequest request = TakeRequest.newBuilder().setPattern(pattern).build();
     TakeResponse response = stub.take(request);
 
@@ -40,6 +54,10 @@ public class ClientService {
   }
 
   public GetTupleSpacesStateResponse getTupleSpacesState() {
+    if (debug) {
+      System.err.println("Getting tuple spaces state");
+    }
+    
     GetTupleSpacesStateRequest request = GetTupleSpacesStateRequest.getDefaultInstance();
     GetTupleSpacesStateResponse response = stub.getTupleSpacesState(request);
 

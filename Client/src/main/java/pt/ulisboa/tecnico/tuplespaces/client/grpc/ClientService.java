21c87f66c8d0f54afee31fd22f9d2212cafd6429
Joaquim Bação <joaquimluzbacao@tecnico.ulisboa.pt>
2024-03-15 16:00:40 +0000

    fix: get tuple spaces

--- Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/grpc/ClientService.java
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/grpc/ClientService.java
@@ -4,11 +4,18 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
-import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.*;
+import pt.ulisboa.tecnico.tuplespaces.centralized.contract.TupleSpacesGrpc;
+import pt.ulisboa.tecnico.tuplespaces.centralized.contract.GetTupleSpacesStateRequest;
+import pt.ulisboa.tecnico.tuplespaces.centralized.contract.GetTupleSpacesStateResponse;
 import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.TupleSpacesReplicaGrpc;
+import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.PutRequest;
+import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.PutResponse;
+import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.ReadRequest;
+import pt.ulisboa.tecnico.tuplespaces.replicaXuLiskov.contract.ReadResponse;
 import pt.ulisboa.tecnico.nameserver.contract.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -20,24 +27,29 @@ public class ClientService {
 
   private final NamingServerServiceGrpc.NamingServerServiceBlockingStub namingServerStub;
   private List<TupleSpacesReplicaGrpc.TupleSpacesReplicaStub> tupleSpacesStubs;
+  private List<TupleSpacesGrpc.TupleSpacesStub> tupleSpacesBlockingStubs;
+  private List<String> tupleSpacesQualifiers;
   private boolean debug = false;
   private static final String TUPLE_SPACES = "TupleSpaces";
-  
 
   public ClientService(String host, String port, boolean debug) {
     this.debug = debug;
 
     final String namingServer_target = host + ":" + port;
     // Set up naming server gRPC stub
-    final ManagedChannel namingServerChannel = ManagedChannelBuilder.forTarget(namingServer_target).usePlaintext().build();
+    final ManagedChannel namingServerChannel = ManagedChannelBuilder.forTarget(namingServer_target).usePlaintext()
+        .build();
     this.namingServerStub = NamingServerServiceGrpc.newBlockingStub(namingServerChannel);
 
     tupleSpacesStubs = new ArrayList<>();
-    lookup(TUPLE_SPACES, ""); //TODO: fazer lookup antes de cada put, read, take etc?
+    tupleSpacesBlockingStubs = new ArrayList<>();
+    tupleSpacesQualifiers = new ArrayList<>();
+
+    lookup(TUPLE_SPACES, ""); // TODO: fazer lookup antes de cada put, read, take etc?
 
   }
 
-  public void put(String tuple) { //TODO: e se nao todos receberem? nao pode ficar so infinitamente a reenviar
+  public void put(String tuple) { // TODO: e se nao todos receberem? nao pode ficar so infinitamente a reenviar
     if (debug) {
       System.err.println("Putting tuple: " + tuple);
     }
@@ -49,145 +61,160 @@ public class ClientService {
 
     // Send the put request to all replicas
     for (TupleSpacesReplicaGrpc.TupleSpacesReplicaStub stub : tupleSpacesStubs) {
-        futures.add(sendPutRequest(stub, request));
+      futures.add(sendPutRequest(stub, request));
     }
 
     // Wait for all replicas to acknowledge
     boolean allDone = false;
     while (!allDone) {
-        try {
-            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get(5, TimeUnit.SECONDS);
-            allDone = true;
-        } catch (InterruptedException | ExecutionException e) {
-            System.out.println("Caught exception: " + e.getMessage());
-            return; // dizer que foi erro?
-        } catch (TimeoutException e) {
-            System.out.println("Timeout reached, resending requests to unacknowledged stubs.");
+      try {
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get(5, TimeUnit.SECONDS);
+        allDone = true;
+      } catch (InterruptedException | ExecutionException e) {
+        System.out.println("Caught exception: " + e.getMessage());
+        return; // dizer que foi erro?
+      } catch (TimeoutException e) {
+        System.out.println("Timeout reached, resending requests to unacknowledged stubs.");
 
-            // Resend the request to the stubs that have not yet acknowledged
-            for (int i = 0; i < futures.size(); i++) {
-                CompletableFuture<PutResponse> future = futures.get(i);
-                if (!future.isDone()) {
-                    // This stub has not yet acknowledged, resend the request
-                    futures.set(i, sendPutRequest(tupleSpacesStubs.get(i), request));
-                }
-            }
+        // Resend the request to the stubs that have not yet acknowledged
+        for (int i = 0; i < futures.size(); i++) {
+          CompletableFuture<PutResponse> future = futures.get(i);
+          if (!future.isDone()) {
+            // This stub has not yet acknowledged, resend the request
+            futures.set(i, sendPutRequest(tupleSpacesStubs.get(i), request));
+          }
         }
+      }
     }
     System.out.println("OK");
   }
 
-  private CompletableFuture<PutResponse> sendPutRequest(TupleSpacesReplicaGrpc.TupleSpacesReplicaStub stub, PutRequest request) {
-      CompletableFuture<PutResponse> future = new CompletableFuture<>();
-      stub.put(request, new StreamObserver<PutResponse>() {
+  private CompletableFuture<PutResponse> sendPutRequest(TupleSpacesReplicaGrpc.TupleSpacesReplicaStub stub,
+      PutRequest request) {
+    CompletableFuture<PutResponse> future = new CompletableFuture<>();
+    stub.put(request, new StreamObserver<PutResponse>() {
+      @Override
+      public void onNext(PutResponse response) {
+        // The response is received
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        future.completeExceptionally(t);
+      }
+
+      @Override
+      public void onCompleted() {
+        future.complete(null);
+      }
+    });
+    return future;
+  }
+
+  public String read(String pattern) {
+    // caso nao haja resposta, tentar novamente
+    while (true) {
+      if (debug) {
+        System.err.println("Reading with pattern: " + pattern);
+      }
+      // lista de futuros
+      List<CompletableFuture<String>> futures = new ArrayList<>();
+
+      ReadRequest request = ReadRequest.newBuilder().setSearchPattern(pattern).build();
+
+      // envia pedido de leitura a todos os replicas
+      for (TupleSpacesReplicaGrpc.TupleSpacesReplicaStub stub : tupleSpacesStubs) {
+        CompletableFuture<String> future = new CompletableFuture<>();
+
+        StreamObserver<ReadResponse> responseObserver = new StreamObserver<ReadResponse>() {
           @Override
-          public void onNext(PutResponse response) {
-              // The response is received
+          public void onNext(ReadResponse response) {
+            future.complete(response.getResult());
           }
 
           @Override
           public void onError(Throwable t) {
-              future.completeExceptionally(t);
+            System.out.println("Caught exception with description: " + t.getMessage());
+            future.complete(null);
           }
 
           @Override
           public void onCompleted() {
+            if (!future.isDone()) {
               future.complete(null);
-          }
-      });
-      return future;
-  }
-
-  public String read(String pattern) {
-    //caso nao haja resposta, tentar novamente
-    while (true) {
-        if (debug) {
-            System.err.println("Reading with pattern: " + pattern);
-        }
-        //lista de futuros
-        List<CompletableFuture<String>> futures = new ArrayList<>();
-
-        ReadRequest request = ReadRequest.newBuilder().setSearchPattern(pattern).build();
-
-        //envia pedido de leitura a todos os replicas
-        for (TupleSpacesReplicaGrpc.TupleSpacesReplicaStub stub : tupleSpacesStubs) {
-            CompletableFuture<String> future = new CompletableFuture<>();
-
-            StreamObserver<ReadResponse> responseObserver = new StreamObserver<ReadResponse>() {
-                @Override
-                public void onNext(ReadResponse response) {
-                    future.complete(response.getResult());
-                }
-
-                @Override
-                public void onError(Throwable t) {
-                    System.out.println("Caught exception with description: " + t.getMessage());
-                    future.complete(null);
-                }
-
-                @Override
-                public void onCompleted() {
-                    if (!future.isDone()) {
-                        future.complete(null);
-                    }
-                }
-            };
-
-            try {
-                stub.read(request, responseObserver);
-            } catch (StatusRuntimeException e) {
-                System.out.println("Caught exception with description: " + e.getStatus().getDescription());
-                future.complete(null);
             }
+          }
+        };
 
-            futures.add(future);
+        try {
+          stub.read(request, responseObserver);
+        } catch (StatusRuntimeException e) {
+          System.out.println("Caught exception with description: " + e.getStatus().getDescription());
+          future.complete(null);
         }
-        //espera por uma resposta
-        CompletableFuture<Object> anyFuture = CompletableFuture.anyOf(futures.toArray(new CompletableFuture[0]));
-        //se houver resposta, retorna
-        String result = (String) anyFuture.join();
 
-        if (result != null) {
-            System.out.println("OK");
-            return result;
-        }
+        futures.add(future);
+      }
+      // espera por uma resposta
+      CompletableFuture<Object> anyFuture = CompletableFuture.anyOf(futures.toArray(new CompletableFuture[0]));
+      // se houver resposta, retorna
+      String result = (String) anyFuture.join();
+
+      if (result != null) {
+        System.out.println("OK");
+        return result;
+      }
     }
   }
 
   public String take(String pattern) {
-    /*if (debug) {
-      System.err.println("Taking with pattern: " + pattern);
-    }
-
-    TakePhase1Request request = TakePhase1Request.newBuilder().setSearchPattern(pattern).build();
-    try {
-      TakePhase1Response response = tupleSpacesStubs.takePhase1(request);//TODO: 
-
-      System.out.println("OK");
-      return "TODO - here just to compile";//response.getResult();
-    } catch (StatusRuntimeException e) {
-      System.out.println("Caught exception with description: " + e.getStatus().getDescription());
-      return null;
-    }*/
-    return null; //TODO: remove
+    /*
+     * if (debug) {
+     * System.err.println("Taking with pattern: " + pattern);
+     * }
+     * 
+     * TakePhase1Request request =
+     * TakePhase1Request.newBuilder().setSearchPattern(pattern).build();
+     * try {
+     * TakePhase1Response response = tupleSpacesStubs.takePhase1(request);//TODO:
+     * 
+     * System.out.println("OK");
+     * return "TODO - here just to compile";//response.getResult();
+     * } catch (StatusRuntimeException e) {
+     * System.out.println("Caught exception with description: " +
+     * e.getStatus().getDescription());
+     * return null;
+     * }
+     */
+    return null; // TODO: remove
   }
 
-  public getTupleSpacesStateResponse getTupleSpacesState(String qualifier) {
-    /*if (debug) {
+  public String getTupleSpacesState(String qualifier) {
+    /* if (debug) {
       System.err.println("Getting tuple spaces state");
     }
-    lookup(TUPLE_SPACES, qualifier); //TODO: se qualifier nao for vazio entao retornar o endereço e fazer um blocking stub?
-    getTupleSpacesStateRequest request = getTupleSpacesStateRequest.getDefaultInstance();
-    try {
-      getTupleSpacesStateResponse response = tupleSpacesStubs.getTupleSpacesState(request);//TODO: 
 
+    // find the stub with the given qualifier
+    TupleSpacesGrpc.TupleSpacesStub stub = null;
+    for (int i = 0; i < tupleSpacesQualifiers.size(); i++) {
+      if (tupleSpacesQualifiers.get(i).equals(qualifier)) {
+        stub = tupleSpacesBlockingStubs.get(i);
+        break;
+      }
+    }
+
+    GetTupleSpacesStateRequest request = GetTupleSpacesStateRequest.getDefaultInstance();
+    try {
+      GetTupleSpacesStateResponse response = stub.getTupleSpacesState(request, null);
+      
       System.out.println("OK");
-      return response;
-    } catch (StatusRuntimeException e) {
-      System.out.println("Caught exception with description: " + e.getStatus().getDescription());
+      return "TODO - here just to compile";//response.getResult();
+      } catch (StatusRuntimeException e) {
+      System.out.println("Caught exception with description: " +
+      e.getStatus().getDescription());
       return null;
-    }*/
-    return null;//TODO: remove
+      } */
+      return null;
   }
 
   public void lookup(String serviceName, String qualifier) {
@@ -207,13 +234,18 @@ public class ClientService {
         for (ServerEntry serverEntry : response.getServerEntryList()) {
           ServerAddress address = serverEntry.getAddress();
           if (debug) {
-            System.err.println("Received server entry: " + address.getHost() + ":" + address.getPort() + "-" + serverEntry.getQualifier());
+            System.err.println("Received server entry: " + address.getHost() + ":" + address.getPort() + "-"
+                + serverEntry.getQualifier());
           }
-          ManagedChannel channel = ManagedChannelBuilder.forAddress(address.getHost(), address.getPort()).usePlaintext().build();
+          ManagedChannel channel = ManagedChannelBuilder.forAddress(address.getHost(), address.getPort()).usePlaintext()
+              .build();
           TupleSpacesReplicaGrpc.TupleSpacesReplicaStub stub = TupleSpacesReplicaGrpc.newStub(channel);
+          TupleSpacesGrpc.TupleSpacesStub blockingStub = TupleSpacesGrpc.newStub(channel);
           tupleSpacesStubs.add(stub);
-      }
-      return;
+          tupleSpacesBlockingStubs.add(blockingStub);
+          tupleSpacesQualifiers.add(serverEntry.getQualifier());
+        }
+        return;
       } else {
         System.out.println("No server entry found with service name: " + serviceName + " and qualifier: " + qualifier);
         return;
